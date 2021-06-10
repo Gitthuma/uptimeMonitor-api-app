67c4985825b35a7a50e40544ebce526f6b9a47ef
Gitthuma <george.githuma@gmail.com>
2021-06-10 16:19:12 +0300

    Import node built in module called HTTP.

---
+++ index.js
@@ -0,0 +1,11 @@
+/*
+* Primary file to the API
+*
+*
+*/
+
+//Dependencies
+
+/*Import node built in module called HTTP. This is the HTTP server that lets us listen on ports and respond with data. We will do this using a const and require statement.*/
+
+const http = require('http');
