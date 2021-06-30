9a892af152c1d0fb5a973d02b5055eb8fea61d5e
Gitthuma <george.githuma@gmail.com>
2021-06-30 19:46:48 +0300

    Create a new string to hold stream and append incoming data to it with req.on method.

--- index.js
+++ index.js
@@ -47,7 +47,14 @@ const server = http.createServer(function(req, res){
     
     /*Create a new string decorder. When you create a string decoder, you should tell it what char set to expect and in our case is utf-8. This is the case for most JSON API*/
 
+    /*Node deals heavily with streams. These are bits of information coming in at a time as opposed to all at once. Payloads that come in as part of an HTTP request, come in to the HTTP server as a stream, so we need to collect that stream as it comes in, and then when the stream tells us that we are at the end coalesce it into one coherent thing before we can figure out what that payload is. So before we do that, we need to creat a new string to hold it. We are goin to call it buffer and initialize it to an empty string itself and as new data comes in, we will append it to that empty string. we do that by binding it to an event that the request object emits, and that event is called data. So when the request object emits the event data, we want a call back function data to be called so that the data that is being emited to be passed to this call back. Then the buffer that we created earlier should have the data appended to it through a string decoder. So the buffer is going to be appended with decorder.write plus the data that we receive.*/
+
     const decoder = new StringDecoder('utf-8');
+    const buffer = '';
+    req.on('data', function(data){
+        buffer += decorder.write(data);
+    });
+   
 
 
     /*Send the response. We will use our original response in the function.*/
